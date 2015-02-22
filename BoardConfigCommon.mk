de9b3bac2f4fffb8b69d2ff8d875fd0fcd9defaa
ayysir <ayysir@aospal.com>
2015-02-22 03:53:10 -0500

    msm8974-common: add releasetools
    removes AudioFX since devices use MaxxAudio instead
    
    Signed-off-by: ayysir <ayysir@aospal.com>
    
    Change-Id: I888ed68c896811a71562b2abd00a6d422e781b9d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,6 +171,9 @@ TARGET_LDPRELOAD := libNimsWrap.so
 endif
 endif
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/msm8974-common
+
 # SELinux policies
 # qcom sepolicy
 include device/qcom/sepolicy/sepolicy.mk
